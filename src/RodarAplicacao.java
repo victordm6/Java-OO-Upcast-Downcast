35008a781ac5a3f30983b9a4c58e424fdfe9aafe
victordm6 <victor.dm6@hotmail.com>
2022-07-01 16:35:23 -0300

    :tada: Commit inicial

---
+++ src/RodarAplicacao.java
@@ -0,0 +1,12 @@
+class RodarAplicacao {
+
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario();
+
+        Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Faxineiro();
+
+    }
+}
